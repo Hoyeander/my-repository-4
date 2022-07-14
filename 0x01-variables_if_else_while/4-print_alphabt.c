42f058381623abf4eb9691a6cea61e20c17c63dd
Hoyeander <oyindamolayemisi@gmail.com>
2022-07-14 16:28:22 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,12 +7,13 @@
 int main(void)
 {
 char a;
-for (a = "a"; a <= "z"; a++)
+for (a = 'a'; a <= 'z'; a++)
 {
-if (a == "q" || a == "e")
+if (a == 'q' || a == 'e')
 {
 continue;
 }
 putchar (a);
+}
 return (0);
 }
