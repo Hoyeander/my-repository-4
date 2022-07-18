bb358b95262b2af02d42e3b783929127b7e4ffa2
Hoyeander <oyindamolayemisi@gmail.com>
2022-07-17 17:32:28 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,23 +3,22 @@
 
 
 /**
- *
  * main - Entry point
  *
  * Description : program that prints _putchar, followed by a new line.
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	_putchar('95');
-	_putchar('112');
-	_putchar('117');
-	_putchar('116');
-	_putchar('99');
-	_putchar('104');
-	_putchar('97');
-	_putchar('114');
-	_putchar('10');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
